d0981435c657edfecd3260c6de9fa6a29111ffb0
zjx-immersion <jianxin_home@163.com>
2018-11-22 00:38:41 +0800

    [Jason]add given when then templates

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -9,40 +9,24 @@ public class CashRegisterTest {
 
 
     @Test
-    public void should_print_the_real_purchase() {
-        Purchase purchase = new Purchase(new Item[]{});
-
-        MockPrinter printer = new MockPrinter();
-        CashRegister cashRegister = new CashRegister(printer);
-
-        cashRegister.process(purchase);
-
-        printer.verifyPrintHasBeenCalled();
+    public void should_print_the_real_purchase_when_call_process() {
+        //given
+        //when
+        //then
     }
 
     @Test
-    public void should_print_the_stub_purchase() {
-        StubPurchase purchase = new StubPurchase("expected result");
-
-        MockPrinter printer = new MockPrinter();
-        CashRegister cashRegister = new CashRegister(printer);
-
-        cashRegister.process(purchase);
-
-        printer.verifyPrintHasBeenCalledWith("expected result");
+    public void should_print_the_stub_purchase_when_call_process() {
+        //given
+        //when
+        //then
     }
 
     @Test
-    public void should_verify_with_mockito() {
-        Purchase purchase = mock(Purchase.class);
-        when(purchase.asString()).thenReturn("expected result");
-
-        Printer printer = mock(Printer.class);
-        CashRegister cashRegister = new CashRegister(printer);
-
-        cashRegister.process(purchase);
-
-        verify(printer).print("expected result");
+    public void should_verify_with_process_call_with_mockito() {
+        //given
+        //when
+        //then
     }
 
 }
